fa3752dbe5fd3479537c405cb609b475c6a49f31
Ian Zink <ian.zink@snyk.io>
2025-06-27 21:41:36 +1000

    Initial commit

---
+++ Formula/snyk-autocomplete.rb
@@ -0,0 +1,20 @@
+class SnykAutocomplete < Formula
+  desc "Shell completion scripts for Snyk CLI"
+  homepage "https://github.com/z4ce/snyk-autocomplete"
+  url "https://github.com/yourusername/snyk-autocomplete/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "6f2dc6afda84a0cf0a80612eda7e9402bd3549e8508d98bcb2f45798ada28245"
+  license "MIT"
+  head "https://github.com/z4ce/snyk-autocomplete.git", branch: "main"
+
+  depends_on "snyk"
+
+  def install
+    bash_completion.install "bash/snyk.bash" => "snyk"
+    zsh_completion.install "zsh/_snyk"
+    fish_completion.install "fish/snyk.fish"
+  end
+
+  test do
+    assert_match "snyk", shell_output("bash -c 'source #{bash_completion}/snyk && complete -p snyk'")
+  end
+end 
